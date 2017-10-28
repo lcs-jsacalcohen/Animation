0f39f30b3dfbda5a085f06adc7a03517950a0073
Jose Sacal Cohen <jsacalcohen@lcs-mba3216.lcs.on.ca>
2017-10-28 11:45:03 -0400

    bounce right side

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 
 class Sketch : NSObject {
@@ -9,6 +10,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    var dx : Int
+    
     // This function runs once
     override init() {
         
@@ -17,17 +20,38 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        
+     
+        // Set the change value
+        dx = 2
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
-        x += 1
+        //clean up
+         canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 500, bottomLeftY: 500, width: 500, height: 500)
         
+        
+        // Change position
+        x += dx
+        
+        // Make it bounce at right
+        if x > 500 {//start of the block
+            dx = -2 //move to the left}
+            
+  
+            //Draw an ellipse
+            canvas.fillColor = .black
+            
+        }
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX:   x, centreY: x, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
         
     }
     
 }
+
