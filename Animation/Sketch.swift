594114d67bf125684868ae63426b3e29de0a2b48
Russell Gordon <jsacalcohen@lcs.on.ca>
2018-04-17 21:15:38 -0400

    fini

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,7 +8,18 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
+    var x2 : Int
+    var y2 : Int
+    var x3 : Int
+    var y3 : Int
+    var y4 : Int
     
+    // change the position
+    var a : Double
+    var a2 : Double
+    var a3 : Double
+    var a4 : Double
     // This function runs once
     override init() {
         
@@ -17,6 +28,7 @@ class Sketch : NSObject {
         
         // Set starting position
         x = 250
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -26,9 +38,8 @@ class Sketch : NSObject {
         x += 1
         
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX:   x, centreY: x, width: 50, height: 50)
+        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
     }
     
 }
-
